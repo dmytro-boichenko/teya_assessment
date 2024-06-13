4b9e8e3e3b2008c10a62e6f3de8cb0cc721b2f77
Dmytro Boichenko <dmitriy.boichenko@karhoo.com>
2024-06-13 18:32:45 +0100

    An initial commit

---
+++ src/main/java/com/boichenko/teya/TeyaApplication.java
@@ -0,0 +1,13 @@
+package com.boichenko.teya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeyaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boichenko/teya/TeyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boichenko.teya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
